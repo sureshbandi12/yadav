e4e9c9d515b0c0571f49aca9c34b56513a160e05
sureshbandi12 <sureshyadav2243@gmail.com>
2024-02-28 10:51:46 +0530

    first commit

---
+++ div.js
@@ -0,0 +1,17 @@
+//divisibility rules
+
+var x;
+var x= ("3");
+if (x % 2 == 0){
+    console.log('the number is divisible by 2');
+}
+else if (x %5 ==0){
+    console.log ('the number is divisible by 4');
+}
+else if (x %11==0){
+    console.log('the number is divisible by 11');
+}
+else {
+ console.log('the number is not divisible by any of the numbers above');
+}
+
---
+++ grade.js
@@ -0,0 +1,23 @@
+// create a grade calculator
+var percentage = 58;
+if(percentage> 90 ){
+    console.log('grade is S and your grade points are !0')
+}
+else if (percentage>80 && percentage<=90){
+    console.log('you grade is A and your grade points are 9')
+}
+else if(percentage>70 && percentage<=80){
+    console.log('your grade is B and your grade points is 8')
+}
+else if(percentage>60 && percentage<=70){
+    console.log('your grade is C and your grade points are 7')
+}
+else if(percentage>50 && percentage<=60){
+    console.log('your grade is D and your grade points are 6' )
+}
+else if(percentage>40){
+    console.log('you FAILED')
+}
+else{
+    console.log("NA")
+}
