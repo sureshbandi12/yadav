da8fd9582a72acd786ae2443c7b2ed882dfeafe1
sureshbandi12 <sureshyadav2243@gmail.com>
2024-03-04 12:00:18 +0530

    switch commit

---
+++ sriyaan/practice.js
@@ -0,0 +1,21 @@
+// grade calculator using switch\\
+
+var marks=35;
+if(marks>90){
+    console.log("a grdae")
+}else if(marks>70){
+    console.log("b grade")
+}else if(marks>50){
+    console.log("c grade")
+}else if(marks>36){
+    console.log("d grade")
+}
+else if(marks>30){
+    console.log("e grade")
+}else{
+    console.log("fail")
+}
+
+
+
+         
---
+++ sriyaan/prime.js
@@ -0,0 +1,6 @@
+function isPrime(number) {
+    if (number <= 1 ) {
+        return false;
+    }
+    for(let i=2; i <=Math.sqrt(number); i++) {  }
+    
