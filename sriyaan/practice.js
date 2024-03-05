5f6cbc416ad7af292c8003d9cf59eb87faef9ab5
sureshbandi12 <sureshyadav2243@gmail.com>
2024-03-05 11:20:04 +0530

    recursion commit

--- sriyaan/practice.js
+++ sriyaan/practice.js
@@ -1,23 +1,10 @@
-// grade calculator using switch\\
-
-var marks=60;
-switch(true){
-    case marks>90 : result="a grade";
-    break;
-    case marks>75 : result ="b grade";
-    break;
-    case marks>60 : result ="c grade"
-    break;
-    case marks>45 : result  = " d grade"
-    break;
-    case marks>36 : result = " e grade"
-    break;
-    default : result="fail";
-
-
-}
-console.log(result);
+//Recursions\\
+function helloWorld(){
+    return "hello"
+}console.log("hello")
+  
 
 
 
-         
+
+        
