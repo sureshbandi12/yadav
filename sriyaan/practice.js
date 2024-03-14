93b5661ed3986145eb123b729fc65a94fa2ce358
sureshbandi12 <sureshyadav2243@gmail.com>
2024-03-14 13:08:09 +0530

    string methods commit

--- sriyaan/practice.js
+++ sriyaan/practice.js
@@ -1,19 +1,34 @@
-var movies =['salar', 'animal', 'baubali', 'pushpa', 'oye']
-movies.push ('pokiri', 'maharshi')
-movies.push ('jaanu')
-movies.push('aarya')
-console.log(movies)
-console.log(movies.length)
-movies.pop('aarya')
-console.log(movies)
-console.log(movies.length)
-movies.push('aarya')
-console.log(movies)
-movies.push('65')
-console.log(movies)
-movies.unshift('varsham')
-console.log(movies)
-movies.shift('varsham')
-console.log(movies)
-console.log(movies.slice(1,4))
-console.log(movies.length)     
+//numbers stringg\\
+let x=100;
+let y=107.5;
+let z=10e2;
+console.log(x,y,z);
+// is integer()
+x =10;
+y =1.8;
+z ="x";
+console.log (Number.isInteger(x));
+console.log(Number.isInteger(y));
+console.log(Number.isInteger(z) );
+//parse()
+s="12345"
+console.log(typeof(s));
+console.log(typeof(Number.parseInt(s)))
+//parsefloat
+s ="123.57";
+console.log(typeof(s));
+console.log(typeof(Number.parseFloat(s)));
+//tostring()
+let n =12345;
+console.log(typeof(n));
+console.log(typeof(Number.toString(n)));
+
+
+
+
+
+
+
+
+
+
